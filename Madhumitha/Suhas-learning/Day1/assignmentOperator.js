68dff17276e34530a1b9eb4f8fcf3d8f6ff79635
MadhumithaNagarajan-alt <152258960+MadhumithaNagarajan-alt@users.noreply.github.com>
2024-06-06 11:32:27 +0530

    first commit

--- Madhumitha/Suhas-learning/Day1/assignmentOperator.js
+++ Madhumitha/Suhas-learning/Day1/assignmentOperator.js
@@ -1,7 +1,17 @@
-let a = 2;
-let b = 2;
+// let a = 2;
+// let b = 2;
  
-let summer = ["June", "July", "August"];
-summer[1] = 7;
-console.log(summer);
+// let summer = ["June", "July", "August"];
+// summer[1] = 7;
+// console.log(summer);
 
+let contact = {
+    _age: 36,
+    firstName : "David",
+    lastName : "Taylor",
+    get fullName() {return `${this.firstName} ${ this.lastName}`;},
+    get age() { return this._age;},
+    set age(a) { if( a > 0) this._age = a;}
+};
+let keys = Object.keys(contact);
+console.log(keys);
